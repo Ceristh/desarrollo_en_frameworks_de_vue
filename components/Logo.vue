2b5a0a8b9c5f5c0a64f299fd7f9410859194efd6
cristopher munoz pareja <ceristh@gmail.com>
2021-11-03 09:45:39 -0300

    Initial commit

---
+++ components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+    <img src="/Rick-and-Morty.png" />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <Logo />
+      <h1>Bienvenido a Rick and Morty, Test integración de Nuxt.js en servidor SSR - Cristopher Muñoz</h1>
+    </v-col>
+  </v-row>
+</template>
